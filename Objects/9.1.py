7020eb929ef9a5a8fc816d5ec3b079853d1db95a
admin <omeruzan14@gmail.com>
2022-08-17 11:04:58 +0300

    Just added a little text

--- Objects/9.1.py
+++ Objects/9.1.py
@@ -6,6 +6,6 @@ YearsUntil100 = 100 - Age
 YearWhen100old = datetime.datetime.now() + datetime.timedelta(weeks= YearsUntil100*52)
 YearWhen100oldString = YearWhen100old.strftime("%d/%m/%y")
 print(f"The year is {YearWhen100old.year} and the date is {YearWhen100old.date()} and it look better in this {YearWhen100oldString}")
-
+print("He became older")
 
 
