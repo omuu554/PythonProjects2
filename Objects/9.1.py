bab67dbffe900595a60ee0037541226bb69cd358
admin <omeruzan14@gmail.com>
2022-08-17 14:12:29 +0300

    Functions Work

--- Objects/9.1.py
+++ Objects/9.1.py
@@ -1,8 +1,8 @@
 import datetime
 
 Name = input("Please enter a name: ")
-AgeOfName = int(input("Please enter an age: "))
-YearsUntil100 = 100 - AgeOfName
+Age= int(input("Please enter an age: "))
+YearsUntil100 = 100 - Age
 YearWhen100old = datetime.datetime.now() + datetime.timedelta(weeks= YearsUntil100*52)
 YearWhen100oldString = YearWhen100old.strftime("%d/%m/%y")
 print(f"The year is {YearWhen100old.year} and the date is {YearWhen100old.date()} and it look better in this {YearWhen100oldString}")
