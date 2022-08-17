865e4c593faccfc6f23e00a7cffe1f6756320c0a
admin <omeruzan14@gmail.com>
2022-08-17 14:25:36 +0300

    Functions Work

--- Objects/10.1.py
+++ Objects/10.1.py
@@ -1,7 +1,11 @@
-def MaxNum(x,y,z) :
-   if(x>y and x>z) :
+Num = input("Please enter number one: ")
+Num2 = input("Please enter number two: ")
+Num3 = input("Please enter number three: ")
+
+def MaxNum(x, y, z) :
+   if(x > y and x > z) :
        return x
-   elif (y>x and y>z):
+   elif (y > x and y > z):
        return y
    else :
        return z
