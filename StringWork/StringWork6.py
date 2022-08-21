60753daa0ff08a2d8beb93802a131f3945b3b2d0
admin <omeruzan14@gmail.com>
2022-08-21 12:08:21 +0300

    Working with Strings

--- StringWork/StringWork6.py
+++ StringWork/StringWork6.py
@@ -1,5 +1,8 @@
 String = input("Please enter a string : ")
 
 String = String[0].upper() + String[1:]
+#if(String[0].islower()):
+ #String = String.replace(String[0],chr(ord(String[0])-32))
 
 print(String)
+
